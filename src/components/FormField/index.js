2013a3e37439946c9fd1729963de0fae4cf9ec1b
basilioarth <basilio.arth@gmail.com>
2020-07-30 14:15:49 -0300

    Atualizações do quarto dia de aula

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,33 +1,136 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-function FormField({ label, type, name, value, onChange }) {
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type="color"] {
+    padding-left: 56px;
+  }
+`;
+
+const Label = styled.label``;
+Label.Text = styled.span`
+  color: #E5E5E5;
+  height: 57px;
+  position: absolute; 
+  top: 0;
+  left: 16px;
+  
+  display: flex;
+  align-items: center;
+  
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  
+  transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+  
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+  
+  padding: 16px 16px;
+  margin-bottom: 45px;
+  
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+  
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+
+  &:focus:not([type="color"]) + span {
+    transform: scale(.6) translate(-10px);
+  }
+
+  ${({ value }) => {
+    const hasValue = value.length > 0;
+    return hasValue && css`
+        {/* III */}
+        &:not([type='color']) + ${Label.Text} {
+          transform: scale(.6) translateY(-10px);
+        }
+      `;
+  }}
+`;
+
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `ìd_${name}`;
+  const tag = type === 'textarea' ? 'textarea' : 'input';
+  const hasValue = Boolean(value.length);
+
   return (
-    <div>
-      <label>
-        {label}: 
-        <input
+    <FormFieldWrapper>
+      <Label
+        htmlFor={fieldId}
+      >
+        <Input
+          as={tag}
+          id={fieldId}
           type={type}
           value={value}
           name={name}
+          hasValue={hasValue}
           onChange={onChange}
         />
         {/* I */}
-      </label>
-    </div>
-  )
+        <Label.Text>
+          {label}
+          :
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
 }
 
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+/* II */
 export default FormField;
 
 // Comentários:
 /*
-  I: Uma observação importante é que a função onChange detecta mudanças 
-  não no input,mas sim mudanças no value desse input (no value que estamos 
+  I: Uma observação importante é que a função onChange detecta mudanças
+  não no input,mas sim mudanças no value desse input (no value que estamos
   atribuindo ao input. Nesse caso, mudanças na variável nomeDaCategoria).
-  A função Handler detecta a tentativa do usuário em mudar o valor do input 
-  através da digitação, pega as informações desse evento (eventInfo) e 
-  modifica/atualiza o valor da variável nomeDaCategoria através da função 
-  setNomeDaCategoria. Obs: eventiInfo.target.value se trata justamente da 
-  tecla que foi digitada naquele momento; toda vez que uma tecla é digitada 
+  A função Handler detecta a tentativa do usuário em mudar o valor do input
+  através da digitação, pega as informações desse evento (eventInfo) e
+  modifica/atualiza o valor da variável nomeDaCategoria através da função
+  setNomeDaCategoria. Obs: eventiInfo.target.value se trata justamente da
+  tecla que foi digitada naquele momento; toda vez que uma tecla é digitada
   um evento é acionado.
+  II: Quando algum elemento não é obrigatório (isRequired) precisamos definir
+  um valor default pra ele. Por isso criamos o FormField.defaultProps e colocamos
+  esses elementos lá dentro juntamento com o seu valor default.
+  III: Se o valor antes do '&&' for verdadeiro, retorna o valor que está dps do '&&'.
+  Caso contário, retorna o próprio valor antes do '&&'.
 */
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,49 +2,53 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
-  }
+  };
   const [categorias, setCategorias] = useState([]);
   /* I */
   const [values, setValues] = useState(valoresIniciais);
-
 
   function setValue(chave, valor) {
     setValues({
       ...values,
       [chave]: valor,
       /* V */
-    })
+    });
   }
 
   function handleChange(infosDoEvento) {
     setValue(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
+      infosDoEvento.target.value,
     );
   }
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
       <form onSubmit={function handleSubmit(infosDoEvento) {
-          infosDoEvento.preventDefault();
-          /* II */
-          setCategorias([
-            ...categorias,
-            /* III */
-            values
-            /* IV */
-          ]);
+        infosDoEvento.preventDefault();
+        /* II */
+        setCategorias([
+          ...categorias,
+          /* III */
+          values,
+          /* IV */
+        ]);
 
-          setValues(valoresIniciais)
-      }}>
+        setValues(valoresIniciais);
+      }}
+      >
 
         <FormField
           label="Nome da Categoria"
@@ -55,23 +59,12 @@ function CadastroCategoria() {
         />
 
         <FormField
-          label="Descrição:"
-          type="????"
+          label="Descrição"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
         />
-        {/* <div>
-          <label>
-            Descrição:
-            <textarea
-              type="text"
-              value={values.descricao}
-              name="descricao"
-              onChange={handleChange}
-            />
-          </label>
-        </div> */}
 
         <FormField
           label="Cor"
@@ -80,69 +73,55 @@ function CadastroCategoria() {
           value={values.cor}
           onChange={handleChange}
         />
-        {/* <div>
-          <label>
-            Cor:
-            <input
-              type="color"
-              value={values.cor}
-              name="cor"
-              onChange={handleChange}
-            />
-          </label>
-        </div> */}
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
-      
 
       <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria.nome}
-            </li>
-          )
-        })}
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
       </ul>
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
 
 // Comentários:
-  /*
+/*
   I: O useState retorna um array de dois valores. O primeiro deles é
-  um estado (nesse caso, o array que estamos passando entre parênteses, 
-  pois ele se trata do valor inicial para esse estado) e o segundo é a 
+  um estado (nesse caso, o array que estamos passando entre parênteses,
+  pois ele se trata do valor inicial para esse estado) e o segundo é a
   função que possibilita alterarmos o valor desse estado.
   Com a desfragmentação conseguimos colocar o estado na variável cate-
   gorias e a função na variável setCategorias.
 
-  II: Toda vez que tentarmos submeter um formulário ele não será submetido 
+  II: Toda vez que tentarmos submeter um formulário ele não será submetido
   da forma default. Isso faz com que a página da aplicação não seja recarregada.
 
-  III: Esses '...' antes do array 'categorias' é uma maneira de indicarmos ao 
-  JavaScript para que ele não sobescreva os valores antigos pelos novos valores. 
-  Em outras palavras, estamos dizendo, com os '...', para manter tudo que está 
-  armazenado em 'categorias' + o(s) novo(s) valor(es) passado(s) (no caso, a 
+  III: Esses '...' antes do array 'categorias' é uma maneira de indicarmos ao
+  JavaScript para que ele não sobescreva os valores antigos pelos novos valores.
+  Em outras palavras, estamos dizendo, com os '...', para manter tudo que está
+  armazenado em 'categorias' + o(s) novo(s) valor(es) passado(s) (no caso, a
   variável 'nomeDaCategoria').
 
-  IV: Lembrando que o valor da variável 'nomeDaCategoria' está sendo atualizado e 
-  administrado pela função HandleEvent presente no atributo onChange do input 
+  IV: Lembrando que o valor da variável 'nomeDaCategoria' está sendo atualizado e
+  administrado pela função HandleEvent presente no atributo onChange do input
   responsável por capturar as novas categorias a serem cadastradas.
 
   V: O valor passado como primeiro parâmetro (chave) pode ser várias coisas
   (nome da categoria, descrição da categoria, cor da categoria); queremos que
   essa implementação aja de forma dinânica. Ao envolvermos a variável chave
-  com colchetes definimos nessa linha qual dos campos do objeto iremos atingir 
+  com colchetes definimos nessa linha qual dos campos do objeto iremos atingir
   de acordo com o parâmetro passado na chamada da função.
   Em outras palavras, se for passado 'nome' na chamada da função como primeiro
   parâmetro, será o atributo nome do objeto em questão que será modificado.
